5534225fd557ad5cb8c388d0c937723105290cd2
lpotte <lpotte@SU5-14.uninorteacad.uninorte.edu.co>
2018-09-17 10:51:10 -0500

    100% terminado...

--- DiegoAlborRojas_LuisPotteHerrera/src/diegoalborrojas_luispotteherrera/Main.java
+++ DiegoAlborRojas_LuisPotteHerrera/src/diegoalborrojas_luispotteherrera/Main.java
@@ -5,31 +5,31 @@
  */
 package diegoalborrojas_luispotteherrera;
 
-import java.util.ArrayList;
-
 /**
  *
  * @author lpotte
  */
 public class Main {
 
-    static ArrayList<Vertice> grafo = new ArrayList<>();
     static int inf = 9999999;
-    static int[][] ad = {{0, 1, 1, 0, 1},
-    {1, 0, 1, 1, 1},
-    {1, 1, 0, 1, 1},
-    {0, 1, 1, 0, 0},
-    {1, 1, 1, 0, 0}};
-    static int[][] distancia = {{0, 1, 14, inf, 2},
-    {1, 0, 7, 3, 5},
-    {14, 7, 0, 9, 11},
-    {inf, 3, 9, 0, inf},
-    {2, 5, 11, inf, 0}};
-    static Object[][] caminos = {{null, 1, 2, 3, 4},
-    {0, null, 2, 3, 4},
-    {0, 1, null, 3, 4},
-    {0, 1, 2, null, 4},
-    {0, 1, 2, 3, null}};
+    static int[][] ad = {
+        {0, 1, 1, 0, 1},
+        {1, 0, 1, 1, 1},
+        {1, 1, 0, 1, 1},
+        {0, 1, 1, 0, 0},
+        {1, 1, 1, 0, 0}};
+    static int[][] distancia = {
+        {0, 1, 14, inf, 2},
+        {1, 0, 7, 3, 5},
+        {14, 7, 0, 9, 11},
+        {inf, 3, 9, 0, inf},
+        {2, 5, 11, inf, 0}};
+    static Object[][] caminos = {
+        {null, 1, 2, 3, 4},
+        {0, null, 2, 3, 4},
+        {0, 1, null, 3, 4},
+        {0, 1, 2, null, 4},
+        {0, 1, 2, 3, null}};
 
     public static void FW() {
         int s;
@@ -47,28 +47,29 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Matriz de distancia iniical");for (int i = 0; i < distancia.length; i++) {
-        for (int j = 0; j < distancia.length; j++) {
-                System.out.print("   " + distancia[i][j]);
+        System.out.println("Matriz de distancia iniical: \n");
+        for (int i = 0; i < distancia.length; i++) {
+            for (int j = 0; j < distancia.length; j++) {
+                System.out.print("\t" + distancia[i][j]);
                 if (j == distancia.length - 1) {
                     System.out.println("");
                 }
             }
         }
-        System.out.println("Matriz de menor distanica a partir de FW");
+        System.out.println("\n Matriz de menor distanica despues del algoritmo  Floyd-Warshall: \n");
         FW();
         for (int i = 0; i < distancia.length; i++) {
             for (int j = 0; j < distancia.length; j++) {
-                System.out.print("   " + distancia[i][j]);
+                System.out.print("\t" + distancia[i][j]);
                 if (j == distancia.length - 1) {
                     System.out.println("");
                 }
             }
         }
-        System.out.println("Matriz de camino resultante");
+        System.out.println("\n Matriz de camino resultante: \n");
         for (int i = 0; i < caminos.length; i++) {
             for (int j = 0; j < caminos.length; j++) {
-                System.out.print("     " + caminos[i][j]);
+                System.out.print("\t" + caminos[i][j]);
                 if (j == caminos.length - 1) {
                     System.out.println("");
                 }
