f8d8cf9e1d6acefe69c627a063bf799b14d8127a
daalbor <daalbor@SU5-15.uninorteacad.uninorte.edu.co>
2018-09-17 10:26:52 -0500

    Ya :D

--- DiegoAlborRojas_LuisPotteHerrera/src/diegoalborrojas_luispotteherrera/Main.java
+++ DiegoAlborRojas_LuisPotteHerrera/src/diegoalborrojas_luispotteherrera/Main.java
@@ -7,34 +7,56 @@ package diegoalborrojas_luispotteherrera;
 
 import java.util.ArrayList;
 
-
-
 /**
  *
  * @author lpotte
  */
 public class Main {
+
     static ArrayList<Vertice> grafo = new ArrayList<>();
     static int inf = 9999999;
     static int[][] ad = {{0, 1, 1, 0, 1},
-                        {1, 0, 1, 1, 1},
-                        {1, 1, 0, 1, 1},
-                        {0, 1, 1, 0, 0},
-                        {1, 1, 1, 0, 0}};
-    static int[][] distancia ={{0, 1, 14, inf, 2},
-                            {1, 0, 7, 3, 5},
-                            {14, 7, 0, 9, 11},
-                            {inf, 3, 9, 0, inf},
-                            {2, 5, 11, inf, 0}};
-    static Object[][] caminos ={{null, 1,    2,    3, 4},
-                                {0, null,    2,    3, 4},
-                                {0,    1, null,    3, 4},
-                                {0,    1,    2, null, 4},
-                                {0,    1,    2,    3, null}};
-    
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
+    {1, 0, 1, 1, 1},
+    {1, 1, 0, 1, 1},
+    {0, 1, 1, 0, 0},
+    {1, 1, 1, 0, 0}};
+    static int[][] distancia = {{0, 1, 14, inf, 2},
+    {1, 0, 7, 3, 5},
+    {14, 7, 0, 9, 11},
+    {inf, 3, 9, 0, inf},
+    {2, 5, 11, inf, 0}};
+    static Object[][] caminos = {{null, 1, 2, 3, 4},
+    {0, null, 2, 3, 4},
+    {0, 1, null, 3, 4},
+    {0, 1, 2, null, 4},
+    {0, 1, 2, 3, null}};
+
+    public static void FW() {
+        int s;
+        for (int i = 0; i < distancia.length; i++) {
+            for (int j = 0; j < distancia.length; j++) {
+                for (int k = 0; k < distancia.length; k++) {
+                    s = distancia[i][k] + distancia[k][j];
+                    if (s < distancia[i][j]) {
+                        distancia[i][j] = s;
+                        caminos[i][j] = k;
+                    }
+                }
+            }
+        }
     }
-    
+
+    public static void main(String[] args) {
+        System.out.println("Antes");
+        for (int i = 0; i < distancia.length; i++) {
+            for (int j = 0; j < distancia.length; j++) {
+                System.out.print(" " + distancia[i][j]);
+                if (j == distancia.length - 1) {
+                    System.out.println("");
+                }
+            }
+        }
+        FW();
+    }
+
 }
